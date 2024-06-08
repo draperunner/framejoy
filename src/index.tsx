a9d20c4f4dcf8bece087a209c9b14cd106590415
Mats Byrkjeland <matsbyr@gmail.com>
2024-06-08 15:26:56 +0200

    Replace react-scripts with Vite
    Faster, better maintained, fewer dependencies.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/react";
 
 import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 
 const element = document.getElementById("root");
 if (element) {
@@ -15,13 +13,3 @@ if (element) {
     </React.StrictMode>
   );
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
