2137c6d990f3de9f85f78cf374ddd473112fa63a
Mats Byrkjeland <git@byrkje.land>
2025-08-05 21:42:22 +0200

    Add PostHog analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/react";
+import { PostHogProvider } from "posthog-js/react";
 
 import { App } from "./App";
 
@@ -9,7 +10,16 @@ if (element) {
   createRoot(element).render(
     <React.StrictMode>
       <ColorModeScript />
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          defaults: "2025-05-24",
+          persistence: "memory",
+        }}
+      >
+        <App />
+      </PostHogProvider>
     </React.StrictMode>,
   );
 }
